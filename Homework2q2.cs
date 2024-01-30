d22c68f6044d8958303f64d5048d6780e54b593a
bgreen33 <143014502+bgreen33@users.noreply.github.com>
2024-01-29 19:03:59 -0600

    Add files via upload

---
+++ Homework2q2.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please input the first num:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please input the second num:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please input the third num:");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+
+        int smallest = num1;
+
+        if (num2 < smallest)
+            smallest = num2;
+        if(num3 < smallest)
+            smallest = num3;
+
+        Console.WriteLine($"The smallest value is: {smallest}");
+    }
+}
